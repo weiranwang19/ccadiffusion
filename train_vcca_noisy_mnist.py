c029c4e2d5915d3ac017c6bb74f00f8750d2b8fd
weiran <weiranwang@ttic.edu>
2024-10-22 20:50:27 -0500

    vcca working

--- train_vcca_noisy_mnist.py
+++ train_vcca_noisy_mnist.py
@@ -77,10 +77,10 @@ epochs = 20
 checkpoint_every = 1
 writer = SummaryWriter(log_dir=experiment_dir)
 
-model = vcca.VCCA(input_dims=[784, 784], latent_dim_shared=30, latent_dims_private=[0, 0],
+model = vcca.VCCA(input_dims=[784, 784], latent_dim_shared=30, latent_dims_private=[30, 30],
                   output_activations=['sigmoid', 'sigmoid'],
                   recon_loss_types=['mse_fixed', 'mse_fixed'],
-                  dropout_rate=0.5, writer=writer)
+                  dropout_rate=0.2, writer=writer)
 if torch.cuda.is_available():
     model = model.cuda()
 
